510b15b913b7b25406462d2831ccfe40b8946cd7
calu <johan.calu@gmail.com>
2021-08-06 19:14:07 +0200

    layout maken

---
+++ resources/views/layouts/basislayout.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<!-- CSRF token -->
+@csrf
+	
+<title>{{ config('app.name', 'Vleugels') }}</title>
+
+<!-- link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" -->
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/daisyui@1.11.0/dist/full.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/daisyui@1.11.0/dist/themes.css" rel="stylesheet" type="text/css" />
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box" >
+  <div class="flex-1 px-2 mx-2">
+    <span class="text-lg font-bold">
+        <img src = "{{ asset('/img/logovleugels.png') }}" width="88" height="48">
+    </span>
+  </div> 
+  <div class="flex-none hidden px-2 mx-2 lg:flex">
+    
+    @guest
+            <div class="flex items-stretch">
+                <a class="btn btn-ghost btn-sm rounded-btn" href="#">
+                    {{ __('Login') }}
+                </a>
+            </div>
+
+            <div class="flex items-stretch">
+                <a class="btn btn-ghost btn-sm rounded-btn" href="#">
+                    Stel je vraag
+                </a>
+            </div>
+    @else
+        <div>geen guest - dus aangemeld</div>
+	@endguest 
+  </div> 
+
+</div>
+
